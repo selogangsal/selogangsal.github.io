a7e118b14d336f0a0092d4eb0d13eb37fba30779
Dian Ardiansyah <ardian@ganteng.eu>
2016-03-04 12:56:48 -0500

    update token

--- token.sh
+++ token.sh
@@ -1,3 +1,3 @@
 token=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1)
-echo {\"token\":\"$token\"} > /root/selogangsal.github.io/source/token.json
+echo {\"token\":\"$token\"} > /root/selogangsal.github.io/source/unik.json
 ./deploy.sh "update token"
